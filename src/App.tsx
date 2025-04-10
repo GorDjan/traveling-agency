1d8330c42afe71c95e1fafbebb767a1a6f88a3f2
GorDjan <gorduryan7@gmail.com>
2025-04-10 22:52:35 +0400

    starter setted figma desighn copied

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Page from "./components/page";
+
+function App() {
+  return <Page></Page>;
+}
+
+export default App;
---
+++ src/components/Page.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+
+interface PageProps {
+   children: ReactElement;
+
+}
+export default function Page(props: PageProps) {
+  return (
+    <div>{props.children}</div>
+  )
+}
