b6dcf2331b3b5977a88f75c95d4cc263d4e7d8a0
GorDjan <gorduryan7@gmail.com>
2025-04-29 19:48:49 +0400

    Steps section done added Rating & Steps components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero/Hero";
+import Main from "./components/Main";
 import Navigation from "./components/Navigation/Navigation";
 import Page from "./components/Page";
+import Steps from "./components/Steps/Steps";
 
 
 function App() {
-  return <Page>
-    <Header> 
-      <Navigation />
-      <Hero />
-    </Header>
-  </Page>;
+  return (
+  <Page>
+    <>
+      <Header> 
+
+        <Navigation />
+        <Hero />
+
+      </Header>
+      <Main>
+        <Steps />
+      </Main>
+    </>
+  </Page>
+  );
 }
 
 export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface MainProps {
+    children: ReactElement;
+}
+export default function Main(props: MainProps) {
+  return  <main>
+      {props.children}
+    </main>
+  
+}
