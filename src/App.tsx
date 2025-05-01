db0c17f307d91e938efc694df7ae9d95f8f7c3f4
GorDjan <gorduryan7@gmail.com>
2025-05-01 21:46:40 +0400

    ExploreMore, News, Services: Add new components and update existing ones

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import Hero from "./components/Hero/Hero";
 import Main from "./components/Main";
 import Navigation from "./components/Navigation/Navigation";
 import Page from "./components/Page";
+import Services from "./components/Services/Services";
 import Steps from "./components/Steps/Steps";
+import News from "./components/News/News.tsx";
+import ExploreMore from "./components/ExploreMore/ExploreMore.tsx";
 
 
 function App() {
   return (
   <Page>
-    <>
+    
       <Header> 
 
         <Navigation />
@@ -18,8 +21,11 @@ function App() {
       </Header>
       <Main>
         <Steps />
+        <Services />
+        <News />
+        <ExploreMore />
       </Main>
-    </>
+      
   </Page>
   );
 }
